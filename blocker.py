d90fb63c1defa9b0df8851d668980258c64cfd8d
Joseph Francis <josephfrancis100@gmail.com>
2019-08-25 17:43:19 -0500

    Blocks the given websites correctly

--- blocker.py
+++ blocker.py
@@ -3,15 +3,31 @@ import time
 
 host_path = "/private/etc/hosts"
 redirect = "73.232.161.226"
-website_lists = ["www.facebook.com", "facebook.com"]
+web_lists = ["www.facebook.com", "facebook.com", "www.google.com", "google.com"]
 
 now = datetime.now()
 
 while True:
-    if datetime(now.year, now.month, now.day, 8) < now < datetime(now.year, now.month, now.day, 16):
+    if datetime(now.year, now.month, now.day, 8) < now < datetime(now.year, now.month, now.day, 17):
         print("Working hours")
+        with open(host_path, "r+") as file:
+            content = file.read()
+
+            for website in web_lists:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + " " + website + "\n")
+
     else:
-        print("Chill hours")
+        with open(host_path, "r+") as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in web_lists):
+                    file.write(line)
+            file.truncate()
+
     time.sleep(5)
 
 
